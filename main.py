e82161ce0be5e5212b443762b421d0021e8decf1
Beth Forsyth <beth.forsyth@metaswitch.com>
2019-11-25 18:22:26 +0000

    Adding basic reproduction structure

--- main.py
+++ main.py
@@ -5,31 +5,47 @@ class organism:
         self.code = "0001011010101"
         self.size = 10
         self.health = 5
+        self.dead = False
+
+class organisms:
+    def __init__(self):
+        self.organisms = [organism() for i in range(1)]
 
     def death(self):
-        if self.health < 4:
-            return True
+        for num in range(len(self.organisms)):
+            my_organism = self.organisms[num]
+            if my_organism.health < 4:
+                logging.debug("Dying")
+                my_organism.dead = True
 
     def reproduce(self):
-        if self.health > 6:
-            return True
+        for num in range(len(self.organisms)):
+            my_organism = self.organisms[num]
+            if (my_organism.health > 6 and my_organism.dead )== False:
+                logging.debug("Reproducing")
+                self.organisms.append(organism())
+                return True
 
 class environment:
 
     def __init__(self):
         self.food = 3
 
-    def live(self, organism):
-        organism.health += self.food
-        self.food -= 1
+    def live(self, organisms):
+        for num in range(len(organisms.organisms)):
+            organism = organisms.organisms[num]
+            organism.health += self.food
+            self.food -= 1
 
 
-my_organism = organism()
+logging.basicConfig(level=logging.DEBUG)
+logging.debug("Starting!")
+
+my_organisms = organisms()
 my_env = environment()
 
 for years in range(10):
-    my_env.live(my_organism)
-    if organism.death == True:
-        break
-    elif organism.reproduce == True:
-        logging.debug("reproducing!")
+    logging.debug("loop number {}!".format(years))
+    my_env.live(my_organisms)
+    my_organisms.death()
+    my_organisms.reproduce()
