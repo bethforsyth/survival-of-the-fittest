0dd73842b66b6ec9858b77130a827368baf884da
Beth Forsyth <beth.forsyth@metaswitch.com>
2019-11-25 21:12:54 +0000

    Add initial pulling genes out

--- main.py
+++ main.py
@@ -1,13 +1,56 @@
 import logging
 import copy
+import random 
 
 class organism:
     def __init__(self):
-        self.code = "0001011010101"
+        self.code = []
+        for j in range(random.randint(100,1000)):
+            self.code.append(random.randint(0,9))
+        logging.debug("Code is {}".format(self.code))
         self.size = 10
         self.health = 5
         self.dead = False
 
+    def get_genes(self):
+        genes = [[]]
+        start_pos = 0
+        end_pos = 0
+        codon = [0,0,0]
+        in_gene = False
+        for i in range (len(self.code)):
+            codon[(i%3)]= self.code[i]
+            if i%3 == 2:
+                ## Got a full codon, check what its doing
+                if is_start_codon(codon) and in_gene == false:
+                    in_gene = True
+                    start_pos = i-3
+                elif is_end_codon(codon) and in_gene == true:
+                    end_pos = i
+                    in_gene = False
+                    genes.append = [self.code[start_pos:end_pos]]
+                    start_pos = 0
+                    end_pos = 0
+
+        return genes
+
+
+
+## This should give us the start of a gene approximately
+## 1/100 codons. 
+def is_start_codon(codon):
+    if codon[0] == 0 and codon[1] == 1:
+        True
+    else:
+        False
+
+## This should give us the end of a gene approximately
+## 1/100 codons. 
+def is_end_codon(codon):
+    if codon[0] == 9 and codon[1] == 8:
+        True
+    else:
+        False
 
 class orgs:
     def __init__(self):
@@ -33,8 +76,8 @@ class orgs:
         return
 
     def translation(self):
-        # Turn the genetic code into characteristics.
-        return
+        for org in self.organisms:
+            org.get_genes()
 
 
 class environment:
