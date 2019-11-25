531cb3b596cd3d9d2e82b1cf3328e0e67c6d0e20
Ben Trigg <ben.trigg@metaswitch.com>
2019-11-25 19:43:28 +0000

    Refactor organisms

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 
 class organism:
     def __init__(self):
@@ -7,29 +8,46 @@ class organism:
         self.health = 5
         self.dead = False
 
-class organisms:
+
+class orgs:
     def __init__(self):
         self.organisms = [organism() for i in range(1)]
 
     def death(self):
-        for num in range(len(self.organisms)):
-            my_organism = self.organisms[num]
-            if my_organism.health < 4:
+        list_index = 0
+        for org in self.organisms:
+            if org.health <= 0:
                 logging.debug("Dying")
-                my_organism.dead = True
+                del self.organisms[list_index]
+            else:
+                list_index += 1
 
     def reproduce(self):
-        for num in range(len(self.organisms)):
-            my_organism = self.organisms[num]
-            if (my_organism.health > 6 and my_organism.dead )== False:
+        for org in self.organisms:
+            if org.health > 6:
                 logging.debug("Reproducing")
-                self.organisms.append(organism())
-                return True
+                new_org = copy.deepcopy(org)
+                self.organisms.append(new_org)
+
+    def mutate(self):
+        return
+
+    def translation(self):
+        # Turn the genetic code into characteristics.
+        return
+
 
 class environment:
 
     def __init__(self):
         self.food = 3
+
+    def main(self, organisms):
+        # Anything in environment that needs to change (e.g. plants grow)
+
+        # Environment acts on organisms
+        return
+
 
     def live(self, organisms):
         for num in range(len(organisms.organisms)):
@@ -38,14 +56,25 @@ class environment:
             self.food -= 1
 
 
+
 logging.basicConfig(level=logging.DEBUG)
 logging.debug("Starting!")
 
-my_organisms = organisms()
-my_env = environment()
-
+environ = environment()
+organisms = orgs()
 for years in range(10):
     logging.debug("loop number {}!".format(years))
-    my_env.live(my_organisms)
-    my_organisms.death()
-    my_organisms.reproduce()
+    organisms.mutate()
+    organisms.translation()
+
+    environ.main(organisms)
+
+    organisms.death()
+    organisms.reproduce()
+
+
+
+
+
+
+
