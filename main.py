87deb861dfad668c7eda0d0b5a5ab0b853635fd5
Ben Trigg <ben.trigg@metaswitch.com>
2019-11-25 17:48:41 +0000

    Tabs to spaces

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import logging
 
-
-
 class organism:
-	def __init__(self):
-		self.code = "0001011010101"
-		self.size = 10
-		self.health = 5
+    def __init__(self):
+        self.code = "0001011010101"
+        self.size = 10
+        self.health = 5
 
-	def death(self):
-		if self.health < 4:
-			return true
+    def death(self):
+        if self.health < 4:
+            return true
 
-	def reproduce(self):
-		if self.health > 6:
-			return true
+    def reproduce(self):
+        if self.health > 6:
+            return true
 
 class environment:
 
-	def __init__(self):
-		self.food = 3
+    def __init__(self):
+        self.food = 3
 
     def live(self, organism):
         organism.health += self.food
@@ -30,8 +28,8 @@ my_organism = organism()
 my_env = environment()
 
 for (years in range(10)):
-	my_env.live(organism)
-	if organism.death == true:
-		break
-	elif organism.reproduce== true:
-		logging.debug("reproducing"!)
+    my_env.live(organism)
+    if organism.death == true:
+        break
+    elif organism.reproduce== true:
+        logging.debug("reproducing"!)
