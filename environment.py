3e93036c2a6a927e03f31387ee89dfaa05201cf2
Radek Hromadka <radek.hromadka@metaswitch.com>
2019-11-25 21:27:06 +0000

    FUnction for creating grid

--- environment.py
+++ environment.py
@@ -1,53 +1,17 @@
-<<<<<<< HEAD
-envir = []
-class position:
-    def __init__(self, location_id, x, y):
-        self.location=[location_id, x, y]
+class locations:
+    def __init__(self, x, y):
+        self.location=[x,y]
 
+grid = []
 counter1=0
 counter2=0
-counter3=0
-locations=[]
-while counter2<21:
-    locations.append(position(counter3, counter1, counter2))
-    counter1+=1
-    counter3+=1
-    envir=[]
-
-    if counter1>20:
-        counter1-=20
-        counter2+=1
-
-
-# class position:
-#     def __init__(self, location_id, x, y, temperature=0):
-#         self.location=[location_id, x, y, temperature]
-
-month = 1
-if month > 12:
-    month-=12
-
-temperature = 20-((month-6)^2)/4
-
-counter1=0
-counter2=0
-counter3=0
-
-
-# while counter2<21:
-#     environment.append(position(counter3, counter1, counter2).location)
-#     counter1+=1
-#     counter3+=1
-
-#     if counter1>20:
-#         counter1-=20
-#         counter2+=1
-
-print(locations)
-=======
-class location():
-    """docstring for location"""
-    def __init__(self, arg):
-        super(location, self).__init__()
-        self.arg = arg
->>>>>>> 435439dd3a84f4e748fac985101502c54a12f915
+xrow=[]
+while counter1 < 20:
+    xrow.append(locations(counter1,counter2))
+    counter2 += 1
+    if counter2 > 20:
+        counter2 = 0
+        counter1 += 1
+        grid.append(xrow)
+        xrow=[]
+print(grid)
