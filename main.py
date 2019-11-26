98456700d0c61c49a9fc67f51699850438e9b639
Ben Trigg <ben.trigg@metaswitch.com>
2019-11-26 13:17:07 +0000

    Resolve refactoring

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ class organism:
         self.genes = []
         start_pos = 0
         end_pos = 0
-        codon = [0, 0, 0]
         in_gene = False
         for i in range(2, len(self.code)):
             if is_start_codon(self.code[i - 2], self.code[i - 1]) and not in_gene:
+                analyse_gene()
                 start_pos = i - 2
-            elif is_end_codon(codon) and in_gene:
+            elif is_end_codon(self.code[i - 2], self.code[i - 1]) and in_gene:
                 end_pos = i
                 in_gene = False
                 self.genes.append(self.code[start_pos:end_pos])
@@ -39,8 +39,8 @@ def is_start_codon(first_base, second_base):
 
 
 # This should give us the end of a gene approximately 1/100 codons.
-def is_end_codon(codon):
-    if codon[0] == 9 and codon[1] == 8:
+def is_end_codon(first_base, second_base):
+    if first_base == 0 and second_base == 1:
         return True
     else:
         return False
